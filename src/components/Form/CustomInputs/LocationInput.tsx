60e2cd2bb95fc39a78acd1c743521ac1f201d8ee
Graziano Pascale <pascaleg99@gmail.com>
2022-10-23 14:53:50 -0300

    added google places input

--- src/components/Form/CustomInputs/LocationInput.tsx
+++ src/components/Form/CustomInputs/LocationInput.tsx
@@ -1,1 +1,18 @@
-export const LocationInput = (props: any) => <></>;
+import React from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
+export const LocationInput = (props: any) => (
+  <GooglePlacesAutocomplete
+    placeholder="Search"
+    onPress={(data, details = null) => {
+      // 'details' is provided when fetchDetails = true
+      console.log(data, details);
+    }}
+    query={{
+      key: 'YOUR API KEY',
+      language: 'en',
+    }}
+    currentLocation={true}
+    currentLocationLabel="Current location"
+  />
+);
